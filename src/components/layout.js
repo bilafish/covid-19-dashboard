a4905e2fb7dd171fa2a26145d2e5275a9cfcc1f4
Jason <bilafish@gmail.com>
2020-03-29 06:02:56 +0800

    :lipstick: Remove layout css styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -35,7 +34,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Built with 💜
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
