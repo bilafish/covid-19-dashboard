b730e139ff81baf8c179f9da962532657e118b2b
Jason <bilafish@gmail.com>
2020-04-26 13:04:46 +0800

    🗃 Update global time series data

--- src/components/linechart.js
+++ src/components/linechart.js
@@ -106,6 +106,7 @@ const data = [
   { date: "2020-04-22", confirmed: 2623413, deaths: 183025 },
   { date: "2020-04-23", confirmed: 2708884, deaths: 190857 },
   { date: "2020-04-24", confirmed: 2809977, deaths: 197004 },
+  { date: "2020-04-25", confirmed: 2896744, deaths: 202844 },
 ]
 
 const formatDate = (tickValue) => {
